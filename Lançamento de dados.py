139e5ff18bb436b26a68cd423b85b0c1db3d26b8
mariaholtz <eduhtzcruz@gmail.com>
2026-08-02 17:44:57 -0300

    Update and rename DRG.py to Lançamento de dados.py

--- Lançamento de dados.py
+++ Lançamento de dados.py
@@ -1,7 +1,3 @@
-'''Write a program that simulates rolling a pair of dice. Each time the program runs, it
-should randomly generate two numbers between 1 and 6 (inclusive), representing
-the result of each die. The program should then display the results and ask if the
-user would like to roll again.'''
 
 import random
 rolls = input('Deseja lançar os dados? (sim/nao) ')
