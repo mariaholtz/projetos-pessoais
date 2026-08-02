f79bfc41df761b53506b3dffddb910b98bced32f
mariaholtz <eduhtzcruz@gmail.com>
2026-08-02 17:47:30 -0300

    Update Jogo de pedra, papel ou tesoura.py

--- Jogo de pedra, papel ou tesoura.py
+++ Jogo de pedra, papel ou tesoura.py
@@ -1,8 +1,3 @@
-'''Write a program to simulate a game of Rock, Paper, Scissors.
-The game will prompt the player to choose rock, paper, or scissors by typing 'r',
-'p', or 's'. The computer will randomly select its choice. The game will then display
-both choices using emojis and determine the winner based on the rules.'''
-
 import random
 import emoji 
 
